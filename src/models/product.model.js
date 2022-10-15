9f5a069c5739c5021994ab8219b19af151b84d92
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-10-15 11:21:12 +0100

    Create the product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,48 +1,51 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
-  },
-  interestedUsers: {
-    type: Array,
-    default: [],
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  isSwappable: {
-    type: Boolean,
-    default: false,
-  },
-  condition: {},
-  descriptions: {
-    type: String,
-    required: true,
-  },
-  images: [
-    {
+const productSchema = mongoose.Schema(
+  {
+    name: {
       type: String,
       required: true,
     },
-  ],
-  isAvailable: {
-    type: Boolean,
-    default: false,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    interestedUsers: {
+      type: Array,
+      default: [],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    isSwappable: {
+      type: Boolean,
+      default: false,
+    },
+    condition: {},
+    descriptions: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    isAvailable: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model('Product', productSchema);
 
