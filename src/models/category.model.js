8afba65cf4a654271ea6390ef010723eb822c5c7
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-10-15 11:27:05 +0100

    Create the category model

---
+++ src/models/category.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
