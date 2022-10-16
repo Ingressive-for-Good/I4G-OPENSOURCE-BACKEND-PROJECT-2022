cd71973306df4ba7aa8f4a23606393aa9ebc249d
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-10-16 23:26:05 +0100

    Create the category feature

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const { cloudinary } = require('./utils/helpers')
+const categoryRoutes = require('./routes/category.route')
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }))
     .use(helmet())
 
 app.use('*', cloudinary)
+
+app.use('/categories', categoryRoutes)
 
 app.get('/test', (req, res) => {
     res.status(200).json({
