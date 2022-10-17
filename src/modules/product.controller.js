982a82d1714a7ff4ce1c532d6ff37c86ded4fdd9
spykelion <ndilionel6@gmail.com>
2022-10-17 03:02:19 +0100

    get all interested users : Users should be able to see who is interested in their product.

--- src/modules/product.controller.js
+++ src/modules/product.controller.js
@@ -3,7 +3,7 @@ const Product = require('../models/product.model')
 const Category = require('../models/category.model')
 
 module.exports = {
-    createProducts: async (req, res) => {
+    createProduct: async (req, res) => {
         try {
             const user = req.user // user must be loggend in to perform this request.
             const { name, categoryId, descriptions, images, price } = req.body
@@ -56,12 +56,52 @@ module.exports = {
         }
     },
 
-            // category.....
+    deleteSingleProduct: async(req, res)=>{
+        const {productId} = req.params
+        try {
+            await Product.deleteOne({_id: productId})
+                .then(deleted=>{
+                    res.status(201).json({
+                        ...deleted,
+                        message: "product has been deleted.",
+                        status: 201
+                    })
+                })
         } catch (error) {
-
+            return res.status(501).json({
+                message: "Could not delete product.",
+                status: 404,
+                ...error
+            })  
         }
     },
-    
+
+    interestedUsers: async (req, res) => {
+        const {productId} = req.params
+        const user = req.user
+        try {
+            await Product.find({user: user._id, _id: productId})
+                .lean()
+                .select("name category interestedUsers")
+                .then(result=>{
+                    return res.status(200).json(result)
+                })
+                .catch(err=>{
+                    return res.status(501).json({
+                        message: "Could not fetch products.",
+                        status: 404,
+                        ...err
+                    })
+                })         
+        } catch (error) {
+            return res.status(501).json({
+                message: "Could not get products.",
+                status: 404,
+                ...error
+            }) 
+        }
+        
+    }
 }
 
 function validateProduct(user, name, categoryId, descriptions, images, price) {
