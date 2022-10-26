cb0fa8d9397f6ddf85dc9124c6087f8ef0f77704
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-10-26 08:59:00 +0100

    Fix some errors

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -5,6 +5,16 @@ const { cloudinary } = require('../../utils/helpers')
 
 module.exports = {
     createProduct: async (req, res) => {
+        if (
+            !req.body.name ||
+            !req.body.description ||
+            !req.body.price ||
+            !req.body.condition
+        ) {
+            return res
+                .status(400)
+                .send({ message: 'Required fields are missing' })
+        }
         try {
             req.body.images = []
             for (const file of req.files) {
@@ -19,18 +29,19 @@ module.exports = {
             if (req.body.images.length === 0) {
                 return res
                     .status(400)
-                    .send({ message: 'Required fields are missing' })
+                    .send({ message: 'Images field are required' })
+            }
+            if (!mongoose.Types.ObjectId.isValid(req.body.category)) {
+                return res.status(400).send({ message: 'Invalid category id' })
             }
             const category = await categoryService.findCategoryById(
                 req.body.category
             )
-            if (!mongoose.Types.ObjectId.isValid(category)) {
-                return res
-                    .status(400)
-                    .send({ message: `Invalid id ${category} ` })
+            if (!category) {
+                return res.status(404).send({ message: 'Category not found' })
             }
             const product = await productService.createProduct(
-                req.user,
+                req.body.user,
                 req.body
             )
             res.status(201).send(product)
