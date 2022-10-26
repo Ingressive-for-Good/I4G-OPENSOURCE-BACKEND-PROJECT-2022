a2cb74a5f6abe244b273fecb6d20b261ef903559
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2022-10-26 11:16:40 +0100

    Add multer error handler

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -18,6 +18,10 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
-const upload = multer({ storage, fileFilter, limits: 5 * 1024 * 1024 })
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 },
+})
 
 module.exports = upload
