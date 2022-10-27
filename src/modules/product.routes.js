418586a86c5c14f74424b1916daecdaeba4b33fd
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-27 10:30:43 +0100

    feat: setup get product by id route

--- src/modules/product.routes.js
+++ src/modules/product.routes.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
-const { createProduct, deleteSingleProduct, interestedUsers } = require('./product.controller')
-const {verifyUser} = require('./user.controller')
+const {
+    createProduct,
+    deleteSingleProduct,
+    interestedUsers,
+    getSingleProduct,
+} = require('./product.controller')
+const { verifyUser } = require('./user.controller')
 
-router.post('/create', verifyUser, createProduct)
-      .get('/:productId/interested-users', verifyUser, interestedUsers)
-      .delete('/:productId', verifyUser, deleteSingleProduct)
+router
+    .post('/create', verifyUser, createProduct)
+    .get('/:productId/interested-users', verifyUser, interestedUsers)
+    .get('/:productId', verifyUser, getSingleProduct)
+    .delete('/:productId', verifyUser, deleteSingleProduct)
 
-module.exports = router     
+module.exports = router
