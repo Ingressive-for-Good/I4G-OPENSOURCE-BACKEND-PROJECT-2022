bbf7ff33c7833c48c1d7f64ebee7a8236a908d93
spykelion <ndilionel6@gmail.com>
2022-10-27 19:49:41 +0100

    add route: update product

--- src/modules/product/product.route.js
+++ src/modules/product/product.route.js
@@ -3,6 +3,8 @@ const {
     createProduct,
     getAllProducts,
     deleteProduct,
+    updateProduct,
+    hasImages
 } = require('./product.controller')
 const upload = require('../../utils/multer')
 const multerErrorHandler = require('../../middlewares/multer-errorHandler')
@@ -10,5 +12,6 @@ const multerErrorHandler = require('../../middlewares/multer-errorHandler')
 router.post('/', [upload.array('images', 3), multerErrorHandler], createProduct)
 router.get('/', getAllProducts)
 router.delete('/:productId', deleteProduct)
+router.patch('/:productId', [hasImages, upload.array('images', 3), multerErrorHandler], updateProduct)
 
 module.exports = router
